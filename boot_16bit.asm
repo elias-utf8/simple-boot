57a355b889a3d8dfc5b034404aace180eb440121
elias-utf8 <79870068+elias-utf8@users.noreply.github.com>
2025-07-23 20:59:25 +0000

    Update boot_16bit.asm

--- boot_16bit.asm
+++ boot_16bit.asm
@@ -5,35 +5,35 @@
 ; ======================================================================
 
 org 0x7c00					
-							 ; lets load first 512 bytes
+				 ; lets load first 512 bytes
 boot:
-    mov ah,0x0e 			 ; write Character in TTY mode
+    mov ah,0x0e 		 ; write Character in TTY mode
     
-    mov si,hello 			 ; point si register to str memory location
+    mov si,hello 		 ; point si register to str memory location
 .loop1:
-    lodsb 					 ; load first char of SI in AL and increment
-    or al,al 				 ; is AL == 0 ? (end of string)
+    lodsb 			 ; load first char of SI in AL and increment
+    or al,al 			 ; is AL == 0 ? (end of string)
     jz .second_string  		 ; if (al == 0) jump to second string
-    int 0x10 				 ; runs BIOS interrupt 0x10 to print char
+    int 0x10 			 ; runs BIOS interrupt 0x10 to print char
     jmp .loop1
 
 .second_string:
-                             ; second loop - display date
+                             	 ; second loop - display date
     mov si, date
     
 .loop2:
     lodsb
     or al,al
-    jz halt                  ; jump to halt when completely done
+    jz halt                  	 ; jump to halt when completely done
     int 0x10
     jmp .loop2
 
 halt: 
-    cli 					 ; clear interrupt flag
-    hlt                      ; halt execution
+    cli 			 ; clear interrupt flag
+    hlt                      	 ; halt execution
    
 hello: db "Hello world! ",0
 date: db "20/07/2025",0
 times 510 - ($-$$) db 0 	 ; pad remaining 510 bytes with zeroes
-dw 0xaa55 					 ; magic bootloader magic - 
-                             ; marks this 512 byte sector bootable
+dw 0xaa55 			 ; magic bootloader magic - 
+                        	 ; marks this 512 byte sector bootable
