46cae0b2e016e8605adec3baabb2915e1549fd0d
elias <elias.lolo.46@gmail.com>
2025-07-23 10:42:57 +0200

    Minor changes

--- variants/boot_16bit.asm
+++ variants/boot_16bit.asm
@@ -8,33 +8,33 @@
 ; Here we will write two string with two loops to reach
 ; each character of string.
 
-org 0x7c00 ; Lets load first 512 bytes
+org 0x7c00 ; lets load first 512 bytes
 
 boot:
-    mov ah,0x0e ; Write Character in TTY mode
+    mov ah,0x0e ; write Character in TTY mode
     
-    mov si,hello ; Point "si" register to str memory location
+    mov si,hello ; point "si" register to str memory location
 .loop1:
-    lodsb ; Load the first char of SI in AL and increment SI
-    or al,al ; Is AL == 0 ? (end of string)
-    jz .second_string  ; If (al == 0) jump to second string
-    int 0x10 ; Runs BIOS interrupt 0x10 to print char
+    lodsb ; load the first char of SI in AL and increment SI
+    or al,al ; is AL == 0 ? (end of string)
+    jz .second_string  ; if (al == 0) jump to second string
+    int 0x10 ; runs BIOS interrupt 0x10 to print char
     jmp .loop1
 
 .second_string:
-    ; Second loop - display date
+    ; second loop - display date
     mov si, date
     
 .loop2:
     lodsb
     or al,al
-    jz halt  ; Jump to halt when completely done
+    jz halt  ; jump to halt when completely done
     int 0x10
     jmp .loop2
 
 halt:
-    cli ; Clear interrupt flag
-    hlt ; Halt execution
+    cli ; clear interrupt flag
+    hlt ; halt execution
    
 hello: db "Hello world! ",0
 date: db "20/07/2025",0
